a2d36e2a4e0c8012700cdf34c983a4fd87ce6e89
AlessioRW <alessiowright@outlook.com>
2025-01-29 15:24:18 +0000

    init commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/AlessioRW/promrw
+
+go 1.22.7
+
+require github.com/prometheus/prometheus v0.301.0
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.61.0 // indirect
+	github.com/robfig/cron v1.2.0
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/protobuf v1.36.0 // indirect
+)
---
+++ pkg/remote_write.go
@@ -0,0 +1,145 @@
+package promrw
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/robfig/cron"
+)
+
+type RemoteWriteClient struct {
+	userAgent     string
+	prometheusURL string
+	metrics       []prompb.TimeSeries
+	cron          *cron.Cron
+}
+
+type Label struct {
+	Name  string
+	Value string
+}
+
+// Create a new Remote Write Client.
+// Write frequency is a Cron Schedule (see https://pkg.go.dev/github.com/robfig/cron?utm_source=godoc for help.
+func NewClient(remoteWriteURL string, userAgent string, writeFrequency string) (RemoteWriteClient, error) {
+
+	client := RemoteWriteClient{
+		prometheusURL: remoteWriteURL,
+		userAgent:     userAgent,
+		metrics:       []prompb.TimeSeries{},
+	}
+
+	client.cron = cron.New()
+	err := client.cron.AddFunc(writeFrequency, client.pushMetrics)
+	if err != nil {
+		slog.Error("error scheduling cron job", slog.Any("error", err))
+		return RemoteWriteClient{}, err
+	}
+	client.cron.Start()
+
+	return client, nil
+}
+
+// Add a Metric to be pushed to Prometheus.
+func (client *RemoteWriteClient) AddMetric(name string, labels []Label) {
+	pLabels := []prompb.Label{}
+	for _, label := range labels {
+		pLabels = append(pLabels, prompb.Label{Name: label.Name, Value: label.Value})
+	}
+
+	metric := prompb.TimeSeries{
+		Labels:  append(pLabels, prompb.Label{Name: "__name__", Value: name}),
+		Samples: []prompb.Sample{},
+	}
+
+	client.metrics = append(client.metrics, metric)
+}
+
+func (client *RemoteWriteClient) getMetric(metricName string) (prompb.TimeSeries, error) {
+	for _, metric := range client.metrics {
+		for _, label := range metric.Labels {
+			if label.Name == "__name__" {
+				if label.Value == metricName {
+					return metric, nil
+				}
+			}
+		}
+	}
+	return prompb.TimeSeries{}, fmt.Errorf("could not find metric with name \"%v\". add one using the AddMetric method", metricName)
+}
+
+// Add a Timeseries point to a Metric, these will be cleared every run of the Cron passed when initalising the client.
+// Timestamp is a Millisecond value from the Unix Epoch.
+func (client *RemoteWriteClient) AddMetricPoint(metricName string, value float64, timestamp int64) error {
+
+	newSample := prompb.Sample{
+		Value:     value,
+		Timestamp: timestamp,
+	}
+
+	metric, err := client.getMetric(metricName)
+	if err != nil {
+		return err
+	}
+
+	metric.Samples = append(metric.Samples, newSample)
+
+	return nil
+}
+
+func (client *RemoteWriteClient) pushMetrics() {
+
+	writeReq := prompb.WriteRequest{
+		Timeseries: client.metrics,
+	}
+
+	data, err := writeReq.Marshal()
+	if err != nil {
+		return
+	}
+
+	var buffer bytes.Buffer
+	gzipWriter := gzip.NewWriter(&buffer)
+	_, err = gzipWriter.Write(data)
+	if err != nil {
+		return
+	}
+	gzipWriter.Close()
+
+	req, err := http.NewRequestWithContext(context.Background(), "POST", client.prometheusURL, &buffer)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-protobuf")
+	req.Header.Set("User-Agent", client.userAgent)
+	req.Header.Set("Content-Encoding", "gzip")
+	req.Header.Set("X-Prometheus-Remote-Write-Version", "0.1.0")
+
+	httpClient := &http.Client{Timeout: 10 * time.Second}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode/100 != 2 {
+		return
+		// stringErr, _ := io.ReadAll(res.Body)
+		// return fmt.Errorf("remote write request failed with status code: %d, error: %v", res.StatusCode, string(stringErr))
+	}
+
+	client.clearMetricSamples()
+}
+
+// clear samples so we don't send repeating data
+func (client *RemoteWriteClient) clearMetricSamples() {
+	for _, metric := range client.metrics {
+		metric.Samples = []prompb.Sample{}
+	}
+}
